1bac2ca8693fa6abef84248aad33e2f046f155ba
Teguh Hofstee <teguhhofstee@gmail.com>
2017-06-21 14:38:05 -0700

    zip_rec implementation, actually need a placeholder zip_rec module in asdl and then expanded out in apply

--- test.lua
+++ test.lua
@@ -27,6 +27,7 @@ Module = mul
        | map(Module m)
        | reduce(Module m)
        | zip
+       | zip_rec
        | stencil(number w, number h)
        | broadcast(number w, number h) #what about 1d broadcast?
 #       | lift # dont know how to treat this yet
@@ -79,7 +80,11 @@ function L.zip()
 end
 
 function L.zip_rec()
-   return L.zip()
+   local function type_func(t)
+	  print('zip_rec type_func')
+   end
+   
+   return T.zip_rec(type_func)
 end
 
 local function binop_type_func(t)
@@ -133,6 +138,22 @@ function L.reduce(m)
 end
 
 function L.apply(m, v)
+   if m.kind == 'zip_rec' then
+	  assert(v.type.kind == 'tuple')
+
+	  local function is_array_type(t)
+		 return t.kind == 'array' or t.kind == 'array2d'
+	  end
+	  
+	  local a = v.type.a
+	  local b = v.type.b
+	  m = L.zip()
+	  while is_array_type(a.t) and is_array_type(b.t) do
+		 v = L.apply(m, v)
+		 m = L.map(m) 
+		 break
+	  end
+   end
    return T.apply(m, v, m.type_func(v.type))
 end
 
@@ -164,12 +185,34 @@ end
    proving grounds
 --]]
 
+-- ([uint32], [uint32]) -> [(uint32, uint32)]
+-- ((uncurry zip) (a, b))
+local a = L.input(L.array2d(L.uint32(), 3, 3))
+local b = L.input(L.array2d(L.uint32(), 3, 3))
+local c = L.apply(L.zip(), L.concat(a, b))
+local d = L.apply(L.zip_rec(), L.concat(a, b))
+
+-- ([[uint32]], [[uint32]]) -> [[(uint32, uint32)]]
+-- (map (uncurry zip) ((uncurry zip) (a, b)))
+local a = L.input(L.array2d(L.array2d(L.uint32(), 3, 3), 5, 5))
+local b = L.input(L.array2d(L.array2d(L.uint32(), 3, 3), 5, 5))
+local c = L.apply(L.map(L.zip()), L.apply(L.zip(), L.concat(a, b)))
+local d = L.apply(L.zip_rec(), L.concat(a, b))
+
+-- ([[[uint32]]], [[[uint32]]]) -> [[[(uint32, uint32)]]]
+-- (map (map (uncurry zip)) (map (uncurry zip) ((uncurry zip) (a, b))))
+local a = L.input(L.array2d(L.array2d(L.array2d(L.uint32(), 3, 3), 5, 5), 7, 7))
+local b = L.input(L.array2d(L.array2d(L.array2d(L.uint32(), 3, 3), 5, 5), 7, 7))
+local c = L.apply(L.map(L.map(L.zip())), L.apply(L.map(L.zip()), L.apply(L.zip(), L.concat(a, b))))
+local d = L.apply(L.zip_rec(), L.concat(a, b))
+
 local I = L.input(L.array2d(L.uint32(), 1920, 1080))
 local taps = L.placeholder(L.array2d(L.uint32(), 3, 3))
 local st = L.apply(L.stencil(3, 3), I)
 local wt = L.apply(L.broadcast(1920, 1080), taps)
-local st_wt = L.apply(L.map(L.zip()), L.apply(L.zip(), L.concat(st, wt)))
--- local st_wt = L.apply(L.zip_rec(), L.concat(st, wt))
+-- local st_wt = L.apply(L.map(L.zip()), L.apply(L.zip(), L.concat(st, wt)))
+local st_wt = L.apply(L.zip_rec(), L.concat(st, wt))
 local m = L.apply(L.chain(L.map(L.map(L.mul())), L.map(L.reduce(L.add()))), st_wt)
 
--- print(inspect(m))
+print(m.type)
+-- print(inspect(m.type))
