f13616f14382584e9c9ab0d0af3607ad8e5b1536
Teguh Hofstee <teguhhofstee@gmail.com>
2017-07-05 23:46:48 -0700

    whitespace

--- examples/conv.lua
+++ examples/conv.lua
@@ -8,10 +8,10 @@ local I = input(array2d(uint8(), im_size[1], im_size[2]))
 local pad = pad(2, 1, 8, 8)(I)
 local st = stencil(4, 4)(pad)
 local taps = const(array2d(uint8(), 4, 4), {
-						{  4, 14, 14,  4 },
-						{ 14, 32, 32, 14 },
-						{ 14, 32, 32, 14 },
-						{  4, 14, 14,  4 }})
+					  {  4, 14, 14,  4 },
+					  { 14, 32, 32, 14 },
+					  { 14, 32, 32, 14 },
+					  {  4, 14, 14,  4 }})
 local wt = broadcast(pad_size[1], pad_size[2])(taps)
 local st_wt = zip_rec()(concat(st, wt))
 local conv = chain(map(map(mul())), map(reduce(add())))
