21466b6db0634dd4006c7a5e6b80c5cfbab18085
Teguh Hofstee <teguhhofstee@gmail.com>
2017-07-05 17:19:03 -0700

    update the only test

--- spec/lang_spec.lua
+++ spec/lang_spec.lua
@@ -1,26 +1,10 @@
 local l = require 'lang'
-local inspect = require 'inspect'
 
 describe('test', function()
 			it('tests construction of an image', function()
+				  l.import()
 				  local test_t = array2d(array2d(uint32(), 3, 3), 1920, 1080)
-				  -- print(inspect(test_t))
-				  -- print_type(test_t)
-
 				  local I = l.array2d(l.uint32(), 1920, 1080)
-				  -- print(inspect(I))
-
-				  -- print(inspect(stencil()))
-				  -- print(inspect(apply(stencil(), {I = I, w = 3, h = 3})))
-
-				  -- print_type(stencil())
-				  -- print_type(apply(stencil(), {I = I, w = 3, h = 3}))
-
-				  -- print(inspect(apply(stencil(), { I = I, w = 3, h = 3 })))
-				  -- print(inspect(apply(map(map(mul())), apply(stencil(), { I = I, w = 3, h = 3 }))))
-
 				  local test = map(map(mul()))
-				  setmetatable(test, { __call = function(t, args) return apply(t, args) end })
-				  -- print(inspect(test(apply(stencil(), { I = I, w = 3, h = 3 }))))
 			end)
 end)
