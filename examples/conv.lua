a973f22b46d89cb324814deaaec8016f2968e0fd
Teguh Hofstee <teguhhofstee@gmail.com>
2017-07-06 22:09:25 -0700

    remove debugging prints

--- examples/conv.lua
+++ examples/conv.lua
@@ -5,8 +5,8 @@ L.import()
 local im_size = { 1920, 1080 }
 local pad_size = { 1920+16, 1080+3 }
 local I = input(array2d(uint8(), im_size[1], im_size[2]))
-local pad = pad(2, 1, 8, 8)(I)
-local st = stencil(4, 4)(pad)
+local pad = pad(-8, -1, 16, 3)(I)
+local st = stencil(-1, -1, 4, 4)(pad)
 local taps = const(array2d(uint8(), 4, 4), {
 					  {  4, 14, 14,  4 },
 					  { 14, 32, 32, 14 },
