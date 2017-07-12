c92cc102a32f2dd166fcc5895b934ef69338e216
Teguh Hofstee <teguhhofstee@gmail.com>
2017-07-12 14:09:43 -0700

    remove print statements in conv test

--- examples/conv.lua
+++ examples/conv.lua
@@ -13,8 +13,6 @@ local taps = const(array2d(uint8(), 4, 4), {
 					  { 14, 32, 32, 14 },
 					  {  4, 14, 14,  4 }})
 local wt = broadcast(pad_size[1], pad_size[2])(taps)
-print(st.type)
-print(wt.type)
 local st_wt = zip_rec()(concat(st, wt))
 local conv = chain(map(map(mul())), map(reduce(add())))
 local m = conv(st_wt)
