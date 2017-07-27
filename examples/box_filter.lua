247674bb23e901751dcd6bbe63077f3059c6103d
Teguh Hofstee <teguhhofstee@gmail.com>
2017-07-27 16:58:49 -0700

    add box_filter harness

--- examples/box_filter.lua
+++ examples/box_filter.lua
@@ -1,14 +1,18 @@
 local L = require 'betelgeuse.lang'
 local P = require 'betelgeuse.passes'
+local R = require 'rigelSimple'
 
 -- box filter
-local im_size = { 16, 32 }
+local im_size = { 32, 32 }
 local I = L.input(L.array2d(L.uint8(), im_size[1], im_size[2]))
 local pad = L.pad(8, 8, 2, 1)(I)
 local st = L.stencil(-1, -1, 4, 4)(pad)
 local conv = L.map(L.reduce(L.add()))
 local m = L.crop(8, 8, 2, 1)(conv(st))
 local mod = L.lambda(m, I)
+
+local in_size = { L.unwrap(mod).x.t.w, L.unwrap(mod).x.t.h }
+local out_size = { L.unwrap(mod).f.type.w, L.unwrap(mod).f.type.h }
 
 local elem_size = { 1, 1 }
 local util = P.reduction_factor(mod, elem_size)
@@ -20,3 +24,10 @@ res = P.streamify(res, elem_size)
 res = P.peephole(res)
 print('--- Peephole ---')
 P.rates(res)
+
+R.harness{
+   fn = res,
+   inFile = "box_32.raw", inSize = in_size,
+   outFile = "box_filter", outSize = out_size,
+   earlyOverride = 4800, -- downsample is variable latency, overestimate cycles
+}
