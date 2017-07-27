8e13bc441f6eb7fb2f7411f4a23f530756b901f9
Teguh Hofstee <teguhhofstee@gmail.com>
2017-07-27 15:20:40 -0700

    update example spec

--- spec/example_spec.lua
+++ spec/example_spec.lua
@@ -1,6 +1,10 @@
 local lfs = require 'lfs'
 local dir = lfs.currentdir() .. '/examples/'
 
+for i,a in ipairs(arg) do
+   print('ARG[' .. i .. ']: ' .. a)
+end
+
 describe('tests in the examples directory', function()
 			for iter, dir_obj in lfs.dir(dir) do
 			   if string.find(iter, '.lua') then
