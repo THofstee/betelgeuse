6b0f386c91e75500a48fbd75a5a6fdc9ad477c90
Teguh Hofstee <teguhhofstee@gmail.com>
2017-08-09 15:06:32 -0700

    modify graphviz

--- graphviz.lua
+++ graphviz.lua
@@ -1,188 +1,194 @@
 local Graph
 
 local supported_ext = {
-	ps = true,
-	svg = true,
-	fig = true,
-	png = true,
-	imap = true,
-	cmapx = true,
-	pdf = true
+   ps = true,
+   svg = true,
+   fig = true,
+   png = true,
+   imap = true,
+   cmapx = true,
+   pdf = true
 }
 
 local function pcall_wrap(f, ...)
-	local ok, cont = pcall(f, ...)
+   local ok, cont = pcall(f, ...)
 
-	if not ok then
-		error(cont, 2)
-	end
+   if not ok then
+	  error(cont, 2)
+   end
 
-	return cont
+   return cont
 end
 
 local __Graph = {
-	node = function(self, nodename, label)
-		table.insert(self.nodes.node, {node =nodename, label = label})
-		return self
-	end,
+   node = function(self, nodename, label)
+	  table.insert(self.nodes.node, {node = nodename, label = label})
+	  return self.nodes.node[table.getn(self.nodes.node)]
+   end,
 
-	edge = function(self, ...)
-		local args = {...}
+   edge = function(self, ...)
+	  local args = {...}
 
-		for i = 2, #args do
-			table.insert(self.edges.edge, {prev = args[1], succ = args[i]})
-		end
+	  for i = 2, #args do
+		 table.insert(self.edges.edge, {prev = args[1], succ = args[i]})
+	  end
 
-		return self
-	end,
+	  return self
+   end,
 
-	source = function(self , graphtype , graphname , level)
-		graphtype = graphtype or "digraph"
-		graphname = graphname or "defaultname"
-		level = level or 0
-		local src = graphtype .. " " .. graphname .. " {\n" ..
-			"\tgraph [" .. self.graph.style:expand() .. "]\n" ..
-			"\tnode [" .. self.nodes.style:expand() .. "]\n" ..
-			"\tedge [" .. self.edges.style:expand() .. "]\n"
+   source = function(self , graphtype , graphname , level)
+	  graphtype = graphtype or "digraph"
+	  graphname = graphname or "defaultname"
+	  level = level or 0
+	  local src = graphtype .. " " .. graphname .. " {\n" ..
+		 "\tgraph [" .. self.graph.style:expand() .. "]\n" ..
+		 "\tnode [" .. self.nodes.style:expand() .. "]\n" ..
+		 "\tedge [" .. self.edges.style:expand() .. "]\n"
 
-		local node = self.nodes.node
-		for i = 1, #node do
-			src = src .. ("\t\t%s [label=\"%s\"]\n"):format(node[i].node, node[i].label)
-		end
+	  local node = self.nodes.node
+	  for i = 1, #node do
+		 local inspect = require 'inspect'
 
-		local subgraphs = self.subgraphs
-		for subgraphname , subgraph in pairs(subgraphs) do
-			src = src .. ("\n%s\n"):format( subgraph:source("subgraph" , subgraphname , level + 1) )
-		end
+		 src = src .. ('\t\t%s ['):format(node[i].node)
+		 for k,v in pairs(node[i]) do
+			if k ~= 'node' then src = src .. ('%s="%s",'):format(k, v) end
+		 end
+		 src = src .. (']\n')
+	  end
 
-		local edge = self.edges.edge
-		for i = 1, #edge do
-			src = src .. ("\t\t\t%s -> %s\n"):format(edge[i].prev, edge[i].succ)
-		end
+	  local subgraphs = self.subgraphs
+	  for subgraphname , subgraph in pairs(subgraphs) do
+		 src = src .. ("\n%s\n"):format( subgraph:source("subgraph" , subgraphname , level + 1) )
+	  end
 
-		src = src .. "}"
+	  local edge = self.edges.edge
+	  for i = 1, #edge do
+		 src = src .. ("\t\t\t%s -> %s\n"):format(edge[i].prev, edge[i].succ)
+	  end
 
-		local function split(str)
-			if not str:match("\n$") then
-				str = str .. "\n"
-			end
+	  src = src .. "}"
 
-			local ret = {}
+	  local function split(str)
+		 if not str:match("\n$") then
+			str = str .. "\n"
+		 end
 
-			for w in str:gmatch("(.-)\n") do
-				table.insert(ret, w)
-			end
+		 local ret = {}
 
-			return ret
-		end
+		 for w in str:gmatch("(.-)\n") do
+			table.insert(ret, w)
+		 end
 
-		local lines = split(src)
-		local indentstr = string.rep("\t\t" , level)
-		src = indentstr .. table.concat(lines , "\n" .. indentstr)
+		 return ret
+	  end
 
-		return src
-	end,
+	  local lines = split(src)
+	  local indentstr = string.rep("\t\t" , level)
+	  src = indentstr .. table.concat(lines , "\n" .. indentstr)
 
-	subgraph = function(self , subgraphname)
-		local subgraph = Graph()
-		self.subgraphs[subgraphname] = subgraph
-		return subgraph
-	end,
+	  return src
+   end,
 
-	write = function(self, filename)
-		local file = pcall_wrap(io.open, filename, "w+")
-		file:write(self:source())
-		pcall_wrap(io.close, file)
-		return self
-	end,
+   subgraph = function(self , subgraphname)
+	  local subgraph = Graph()
+	  self.subgraphs[subgraphname] = subgraph
+	  return subgraph
+   end,
 
-	compile = function(self, filename, format, generate_filename)
-		format = format or "pdf"
+   write = function(self, filename)
+	  local file = pcall_wrap(io.open, filename, "w+")
+	  file:write(self:source())
+	  pcall_wrap(io.close, file)
+	  return self
+   end,
 
-		if not supported_ext[format] then
-			local format_aligned = {}
+   compile = function(self, filename, format, generate_filename)
+	  format = format or "pdf"
 
-			for fmt in pairs(supported_ext) do
-				table.insert(format_aligned, fmt)
-			end
+	  if not supported_ext[format] then
+		 local format_aligned = {}
 
-			local flen = #format_aligned
+		 for fmt in pairs(supported_ext) do
+			table.insert(format_aligned, fmt)
+		 end
 
-			table.sort(format_aligned)
+		 local flen = #format_aligned
 
-			error(("Graphviz supports the following output formats: %s, and %s (got `%s')"):format(
-				table.concat(format_aligned, ", ", 1, flen - 1), format_aligned[flen], format))
-		end
+		 table.sort(format_aligned)
 
-		self:write(filename)
+		 error(("Graphviz supports the following output formats: %s, and %s (got `%s')"):format(
+			   table.concat(format_aligned, ", ", 1, flen - 1), format_aligned[flen], format))
+	  end
 
-		generate_filename = generate_filename or ("%s.%s"):format(filename, format)
+	  self:write(filename)
 
-		-- compile dot file
-		local cmd_str = ("dot -T%s %s -o %s"):format(
-			format --[[output format]],
-			filename --[[input dot file]],
-			generate_filename --[[output file]])
+	  generate_filename = generate_filename or ("%s.%s"):format(filename, format)
 
-		local cmd = io.popen(cmd_str, "r")
+	  -- compile dot file
+	  local cmd_str = ("dot -T%s %s -o %s"):format(
+		 format --[[output format]],
+		 filename --[[input dot file]],
+		 generate_filename --[[output file]])
 
-		pcall_wrap(cmd.read, cmd, "*a")
-		pcall_wrap(io.close, cmd)
+	  local cmd = io.popen(cmd_str, "r")
 
-		return self
-	end,
+	  pcall_wrap(cmd.read, cmd, "*a")
+	  pcall_wrap(io.close, cmd)
 
-	render = function(self, filename, format, generate_filename)
-		format = format or "pdf"
-		generate_filename = generate_filename or ("%s.%s"):format(filename, format)
-		self:compile(filename, format, generate_filename)
+	  return self
+   end,
 
-		-- open generated file with `xdg-open`
-		local cmd_str = ("xdg-open %s &"):format(generate_filename)
+   render = function(self, filename, format, generate_filename)
+	  format = format or "pdf"
+	  generate_filename = generate_filename or ("%s.%s"):format(filename, format)
+	  self:compile(filename, format, generate_filename)
 
-		local cmd = io.popen(cmd_str, "r")
+	  -- open generated file with `xdg-open`
+	  local cmd_str = ("xdg-open %s &"):format(generate_filename)
 
-		pcall_wrap(cmd.read, cmd, "*a")
-		pcall_wrap(io.close, cmd)
+	  local cmd = io.popen(cmd_str, "r")
 
-		return self
-	end,
+	  pcall_wrap(cmd.read, cmd, "*a")
+	  pcall_wrap(io.close, cmd)
+
+	  return self
+   end,
 }
 
 local style_index = {
-	update = function(orig_style, styles)
-		for k, v in pairs(styles) do
-			orig_style[k] = v
-		end
-	end,
+   update = function(orig_style, styles)
+	  for k, v in pairs(styles) do
+		 orig_style[k] = v
+	  end
+   end,
 
-	expand = function(tbl)
-		local style_str = ""
+   expand = function(tbl)
+	  local style_str = ""
 
-		for k, v in pairs(tbl) do
-			if k == "fontname" then
-				v = ([["%s"]]):format(v)
-			end
-			style_str = style_str .. (" %s=%s"):format(k, v)
-		end
+	  for k, v in pairs(tbl) do
+		 if k == "fontname" then
+			v = ([["%s"]]):format(v)
+		 end
+		 style_str = style_str .. (" %s=%s"):format(k, v)
+	  end
 
-		return style_str:gsub("^ ", "")
-	end
+	  return style_str:gsub("^ ", "")
+   end
 }
 
 -- pseudo Graph class
 Graph = function()
-	return setmetatable({
-		edges = {
+   return setmetatable({
+		 edges = {
 			edge = {},
 			style = setmetatable({}, {__index = style_index})},
-		nodes = {
+		 nodes = {
 			node = {},
 			style = setmetatable({}, {__index = style_index})},
-		graph = {
+		 graph = {
 			style = setmetatable({}, {__index = style_index})},
-		subgraphs = {},
-	}, {__index = __Graph})
+		 subgraphs = {},
+					   }, {__index = __Graph})
 end
 
 return Graph
